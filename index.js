a78a2bdb7b4f467cc608e40c1dc147bb452dd3e0
A. Ishikawa <a.ishikawa810@gmail.com>
2018-01-20 21:07:10 +0900

    Change index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 exports.subscribe = (event, callback) => {
   const pubsubMessage = event.data;
   const name = pubsubMessage.data ? Buffer.from(pubsubMessage.data, 'base64').toString() : 'World';
+  const age = pubSubMessage.attributes.age ? pubsubMessage.attributes.age : -1;
 
-  console.log(`Hello, ${name}!`);
+  console.log(`Hello, ${name}! You are ${age} old`);
 
   callback();
 };
