c2a5b8407cdceeb49376f9f3f30b450236ec2a43
A. Ishikawa <a.ishikawa810@gmail.com>
2018-01-20 16:17:10 +0900

    Add a sample to subscribe pubsub

---
+++ index.js
@@ -0,0 +1,8 @@
+exports.subscribe = (event, callback) => {
+  const pubsubMessage = event.data;
+  const name = pubsubMessage.data ? Buffer.from(pubsubMessage.data, 'base64').toString() : 'World';
+
+  console.log(`Hello, ${name}!`);
+
+  callback();
+};
